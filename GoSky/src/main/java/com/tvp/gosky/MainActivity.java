14ccefb4076cda881af2dd173fdf4c0ef47ce9b3
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-09 20:35:19 +0300

    Kõik failid eelmisest projektist. Kompileerub

--- GoSky/src/main/java/com/tvp/gosky/MainActivity.java
+++ GoSky/src/main/java/com/tvp/gosky/MainActivity.java
@@ -1,159 +1,282 @@
 package com.tvp.gosky;
 
-import com.tvp.gosky.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
-import android.view.MotionEvent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
-public class MainActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+import com.tvp.gosky.util.AsyncHttpPostTask;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
+public class MainActivity extends Activity implements Camera.PreviewCallback, Camera.ErrorCallback, Camera.ShutterCallback, Camera.PictureCallback, Camera.AutoFocusCallback {
 
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Camera _myCamera;
+    private final Handler _handler = new Handler();
+    private View _view = null;
+    // time interval in seconds
+    private int _interval = 60;
+    private boolean _externalStorageAvailable = false;
+    private boolean _externalStorageWritable = false;
+    private boolean _isTakingPictures = false;
+    private String _uploadScriptUrl = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        final View controlsView = findViewById(R.id.fullscreen_content_controls);
-        final View contentView = findViewById(R.id.fullscreen_content);
-
-        // Set up an instance of SystemUiHider to control the system UI for
-        // this activity.
-        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
-        mSystemUiHider.setup();
-        mSystemUiHider
-                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                    // Cached values.
-                    int mControlsHeight;
-                    int mShortAnimTime;
-
-                    @Override
-                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-                    public void onVisibilityChange(boolean visible) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                            // If the ViewPropertyAnimator API is available
-                            // (Honeycomb MR2 and later), use it to animate the
-                            // in-layout UI controls at the bottom of the
-                            // screen.
-                            if (mControlsHeight == 0) {
-                                mControlsHeight = controlsView.getHeight();
-                            }
-                            if (mShortAnimTime == 0) {
-                                mShortAnimTime = getResources().getInteger(
-                                        android.R.integer.config_shortAnimTime);
-                            }
-                            controlsView.animate()
-                                    .translationY(visible ? 0 : mControlsHeight)
-                                    .setDuration(mShortAnimTime);
-                        } else {
-                            // If the ViewPropertyAnimator APIs aren't
-                            // available, simply show or hide the in-layout UI
-                            // controls.
-                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-                        }
-
-                        if (visible && AUTO_HIDE) {
-                            // Schedule a hide().
-                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
-                        }
-                    }
-                });
-
-        // Set up the user interaction to manually show or hide the system UI.
-        contentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (TOGGLE_ON_CLICK) {
-                    mSystemUiHider.toggle();
-                } else {
-                    mSystemUiHider.show();
-                }
-            }
-        });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(_myCamera != null){
+            _myCamera.release();
+            Log.d(TAG, "Camera released, app paused");
+        }
+        if(_isTakingPictures){
+            handlerRemoveCallbacks();
+        }
+    }
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(_myCamera != null){
+            _myCamera.release();
+            Log.d(TAG, "Camera released, app destroyed");
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        try {
+            if(_myCamera != null){
+                _myCamera.reconnect();
             }
+        } catch (IOException e) {
+            Log.d(TAG, "Error reconnecting to Camera!");
+        }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
             return false;
         }
-    };
+    }
 
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
+    /** A safe way to get an instance of the Camera object. */
+    private Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * This method gets called from the activity_main view when start/stop button is pressed
+     * @param view
+     */
+    public void toggleAction(View view){
+        _view = view;
+        _isTakingPictures = !_isTakingPictures;
+        Button _button = (Button) findViewById(R.id.startStop);
+        EditText _uploadUrl = (EditText) findViewById(R.id.uploadUrl);
+        EditText _intervalSeconds = (EditText) findViewById(R.id.intervalSeconds);
+
+        _uploadScriptUrl = _uploadUrl.getText().toString();
+
+        _interval = Integer.parseInt(_intervalSeconds.getText().toString());
+
+        if(_interval == 0){
+            _interval = 60;
+        }
+
+        Log.d(TAG, "Upload script url set to: " + _uploadScriptUrl);
+        Log.d(TAG, "Time interval set to: " + _interval);
+
+        if(!_isTakingPictures){
+            _button.setText(R.string.start);
+            handlerRemoveCallbacks();
+        } else {
+            _button.setText(R.string.stop);
+            handlerPostDelayed();
+        }
+    }
+
+    private long getIntervalInMillis(){
+        return _interval * 1000;
+    }
+
+    private Runnable _runnable = new Runnable() {
         @Override
         public void run() {
-            mSystemUiHider.hide();
+            takePicture();
+            handlerPostDelayed();
         }
     };
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    private void handlerPostDelayed(){
+        _handler.postDelayed(_runnable, getIntervalInMillis());
+    }
+
+    private void handlerRemoveCallbacks(){
+        _handler.removeCallbacks(_runnable);
+    }
+
+    private void takePicture(){
+
+        if(_view == null){
+            Log.d(TAG, "No view! Cannot take picture");
+            return;
+        }
+
+        if(_myCamera == null){
+            if(checkCameraHardware(_view.getContext())){
+                _myCamera = getCameraInstance();
+            }
+        }
+
+        if(_myCamera != null){
+            try {
+                Log.d(TAG, "Start of taking picture");
+                SurfaceView dummy = new SurfaceView(_view.getContext());
+                _myCamera.setPreviewDisplay(dummy.getHolder());
+                _myCamera.startPreview();
+                _myCamera.setPreviewCallback(this);
+                _myCamera.setErrorCallback(this);
+                _myCamera.autoFocus(this);
+                _myCamera.takePicture(null, null, this);
+                Log.d(TAG, "End of taking picture");
+            } catch (Throwable e){
+                Log.d(TAG, "Exception in takePicture: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        else {
+            Log.d(TAG, "No camera present!");
+        }
+    }
+
+    /** Create a File for saving an image or video */
+    private File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        String directory = Environment.DIRECTORY_PICTURES;
+
+        /*
+         There are two options for getting a storage directory on the SD card:
+         Environment.getExternalStoragePublicDirectory(): this uses a public directory. If the app is uninstalled, the photos will remain.
+         Context.getExternalFilesDir(): this uses a directory which is private to the app. If the app is uninstalled, the photos will also be deleted.
+        */
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(directory), getPackageName());
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d(TAG, "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        Log.d(TAG, "Start onPreviewFrame");
+    }
+
+    @Override
+    public void onError(int i, Camera camera) {
+        Log.d(TAG, "Start onError");
+    }
+
+    @Override
+    public void onShutter() {
+        Log.d(TAG, "Start onShutter");
+    }
+
+    @Override
+    public void onPictureTaken(byte[] bytes, Camera camera) {
+        Log.d(TAG, "Start onPictureTaken");
+
+        if(bytes == null || bytes.length == 0){
+            Log.d(TAG, "Image size is 0, nothing to save.");
+            return;
+        }
+
+        File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+        if (pictureFile == null){
+            Log.d(TAG, "Error creating media file, check storage permissions.");
+            return;
+        }
+
+        try {
+            Log.d(TAG, "File name: " + pictureFile.getAbsolutePath());
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(bytes);
+            fos.close();
+            _myCamera.stopPreview();
+            new AsyncHttpPostTask(_uploadScriptUrl).execute(pictureFile);
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Throwable e){
+            Log.d(TAG, "Unknown error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAutoFocus(boolean b, Camera camera) {
+        Log.d(TAG, "Autofocus");
     }
 }
