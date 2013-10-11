6089239fcdf45221a0d1661911ffef2110bb8755
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-11 14:22:07 +0300

    Test if the build process works

--- GoSky/src/main/java/com/tvp/gosky/MainActivity.java
+++ GoSky/src/main/java/com/tvp/gosky/MainActivity.java
@@ -164,7 +164,7 @@ public class MainActivity extends Activity implements Camera.PreviewCallback, Ca
     private void takePicture(){
 
         if(_view == null){
-            Log.d(TAG, "No view! Cannot take picture");
+            Log.d(TAG, "No view present! Cannot take picture");
             return;
         }
 
