dbc7ee3f4f6ef93f7bdff7fa5cbd6cebc752187c
Rasmus Piho <rasmus.piho@gmail.com>
2013-10-29 14:41:28 +0200

    Do not restrict same origin policy for now. Add files and directories count in json feeds

--- api/files.php
+++ api/files.php
@@ -2,6 +2,9 @@
 // reads files from specified path and returns them as an json-array
 // takes one argument from GET variables, does not work without it
 // the argument should come from http call to directories.php
+
+// allow cross-origin requests
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 if(isset($_GET['dir']) && $_GET['dir'] != ""){
@@ -10,12 +13,18 @@ if(isset($_GET['dir']) && $_GET['dir'] != ""){
     $files = @scandir($dir = '../incoming/' . $directory . '/converted/');
     if ($files && is_array($files)) {
       $result = array();
+      $filesArray = array();
       foreach ($files as $file) {
         if(!is_dir($entry = $dir . $file)){
-          $result['files'][]['name'] = $file;
-          $result['files'][]['path'] = str_replace("../", "", $dir) . $file;
+          $entry = array();
+          $entry['name'] = $file;
+          $entry['path'] = str_replace("../", "", $dir) . $file;
+          $filesArray[] = $entry;
         }
       }
+      $result['files_count'] = count($filesArray);
+      $result['files'] = $filesArray;
+      
       echo json_encode($result);
     } 
   }
